03dc1729b7e085e7f4554536f81250cf942842d5
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2022-10-11 15:16:25 +0200

    improving readability of RomansToInteger

--- src/main/java/RomansToInteger.java
+++ src/main/java/RomansToInteger.java
@@ -1,59 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomansToInteger {
 
     public static int romanToInt(String s) {
+
         int result = 0;
-        while(s.contains("IV")) {
-            result+=4;
-            s = s.replaceFirst("IV", "");
+
+        Map<String, Integer> doublesMap = new HashMap<>();
+        doublesMap.put("IV", 4);
+        doublesMap.put("IX", 9);
+        doublesMap.put("XL", 40);
+        doublesMap.put("XC", 90);
+        doublesMap.put("CD", 400);
+        doublesMap.put("CM", 900);
+
+        Map<String, Integer> singlesMap = new HashMap<>();
+        singlesMap.put("I", 1);
+        singlesMap.put("V", 5);
+        singlesMap.put("X", 10);
+        singlesMap.put("L", 50);
+        singlesMap.put("C", 100);
+        singlesMap.put("D", 500);
+        singlesMap.put("M", 1000);
+
+        for(String key: doublesMap.keySet()) {
+            if(s.contains(key)) {
+                result+=doublesMap.get(key);
+                s = s.replaceFirst(key, "");
+            }
         }
-        while(s.contains("IX")) {
-            result+=9;
-            s = s.replaceFirst("IX", "");
+
+        for(String key: singlesMap.keySet()) {
+            while(s.contains(key)) {
+                result+=singlesMap.get(key);
+                s = s.replaceFirst(key, "");
+            }
         }
-        while(s.contains("XL")) {
-            result+=40;
-            s = s.replaceFirst("XL", "");
-        }
-        while(s.contains("XC")) {
-            result+=90;
-            s = s.replaceFirst("XC", "");
-        }
-        while(s.contains("CD")) {
-            result+=400;
-            s = s.replaceFirst("CD", "");
-        }
-        while(s.contains("CM")) {
-            result+=900;
-            s = s.replaceFirst("CM", "");
-        }
-        while(s.contains("I")) {
-            result+=1;
-            s = s.replaceFirst("I", "");
-        }
-        while(s.contains("V")) {
-            result+=5;
-            s = s.replaceFirst("V", "");
-        }
-        while(s.contains("X")) {
-            result+=10;
-            s = s.replaceFirst("X", "");
-        }
-        while(s.contains("L")) {
-            result+=50;
-            s = s.replaceFirst("L", "");
-        }
-        while(s.contains("C")) {
-            result+=100;
-            s = s.replaceFirst("C", "");
-        }
-        while(s.contains("D")) {
-            result+=500;
-            s = s.replaceFirst("D", "");
-        }
-        while(s.contains("M")) {
-            result+=1000;
-            s = s.replaceFirst("M", "");
-        }
+
         return result;
     }
 }
