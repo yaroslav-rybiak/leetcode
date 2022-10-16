35ac6fd9ab86033d1a3e2cb5b1704457230223aa
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2022-10-16 21:41:14 +0200

    implementing AddBinaryTest

---
+++ src/test/java/AddBinaryTest.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AddBinaryTest {
+
+    @Test
+    public void testAddBinary(){
+        Assert.assertEquals("0", AddBinary.addBinary("0", "0"));
+        Assert.assertEquals("10", AddBinary.addBinary("1", "1"));
+        Assert.assertEquals("10101", AddBinary.addBinary("1010", "1011"));
+        Assert.assertEquals(
+                "110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000",
+                AddBinary.addBinary(
+                        "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
+                        "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"));
+    }
+}
