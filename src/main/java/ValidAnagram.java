33260ebfe5081858f8717fa0becb93b8ae679d8a
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2022-10-22 22:46:28 +0200

    implementing ValidAnagram

---
+++ src/main/java/ValidAnagram.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class ValidAnagram {
+
+    public static boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        char[] sChar = s.toCharArray();
+        char[] tChar = t.toCharArray();
+        Arrays.sort(sChar);
+        Arrays.sort(tChar);
+        return Arrays.equals(sChar, tChar);
+    }
+}
