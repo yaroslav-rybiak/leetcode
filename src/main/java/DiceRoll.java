4b2cd84f5fbe95de7ac3a1ffce6f59fbb20b631b
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2022-10-26 22:59:06 +0200

    implementing dice rolls

---
+++ src/main/java/DiceRoll.java
@@ -0,0 +1,46 @@
+import java.util.Random;
+
+public class DiceRoll {
+
+    public static void main(String[] args) {
+        DiceRoll dr = new DiceRoll();
+        System.out.println(dr.howManyPairs());
+    }
+
+    public int howManyPairs(){
+        Dice d6 = new Dice(6);
+        return d6.rollTwo(10);
+    }
+
+
+    public class Dice {
+        private int numberOfSides;
+
+        public Dice(int numberOfSides) {
+            this.numberOfSides = numberOfSides;
+        }
+
+        public int rollTwo(int numberOfRolls) {
+            int pairs = 0;
+            for(int i=1; i<= numberOfRolls; i++) {
+                Random r = new Random();
+                int firstDice = r.nextInt(6)+1;
+                int secondDice = r.nextInt(5)+1;
+                System.out.println("Roll number " + i);
+                System.out.println("first dice: " + firstDice);
+                System.out.println("second dice: " + secondDice);
+                if(firstDice==secondDice) {
+                    pairs++;
+                    System.out.println("WE HAVE A PAIR ("+pairs+")");
+
+                } else {
+                    System.out.println("Not a pair");
+                }
+                System.out.println();
+            }
+            return pairs;
+        }
+
+
+    }
+}
