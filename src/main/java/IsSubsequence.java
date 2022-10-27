a6b64462fde3a54751e680cea8cf6ef4ff774cac
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2022-10-27 23:42:25 +0200

    Implementing IsSubsequence

--- src/main/java/IsSubsequence.java
+++ src/main/java/IsSubsequence.java
@@ -1,8 +1,11 @@
 public class IsSubsequence {
 
-    public boolean isSubsequence(String s, String t) {
-
-
-        return false;
+    public static boolean isSubsequence(String s, String t) {
+        for(int i = 0; i< s.length(); i++) {
+            int j = t.indexOf(s.charAt(i));
+            if(j==-1) return false;
+            t = t.substring(j+1);
+        }
+        return true;
     }
 }
