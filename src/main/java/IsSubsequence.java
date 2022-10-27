2adc3d9ace7412eab4d83ad3999d9e932a536c6b
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2022-10-27 23:44:52 +0200

    Improving IsSubsequence

--- src/main/java/IsSubsequence.java
+++ src/main/java/IsSubsequence.java
@@ -2,9 +2,8 @@ public class IsSubsequence {
 
     public static boolean isSubsequence(String s, String t) {
         for(int i = 0; i< s.length(); i++) {
-            int j = t.indexOf(s.charAt(i));
-            if(j==-1) return false;
-            t = t.substring(j+1);
+            if(t.indexOf(s.charAt(i))==-1) return false;
+            t = t.substring(t.indexOf(s.charAt(i))+1);
         }
         return true;
     }
